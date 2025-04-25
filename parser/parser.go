148f2f1dd2c44a06d721c66eaf39e21d2e4366bb
Parthib Mukherjee <parthibmukherjee@gmail.com>
2025-04-25 04:43:22 +0000

    added Tracer function to understand the flow of the said parser

--- parser/parser.go
+++ parser/parser.go
@@ -176,6 +176,9 @@ func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 }
 
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+
+	defer untrace(trace("parseExpressionStatement"))
+
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 
 	stmt.Expression = p.parseExpression(LOWEST)
@@ -205,6 +208,8 @@ func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 
 }
 func (p *Parser) parseExpression(precedence int) ast.Expression {
+	defer untrace(trace("parseExpression"))
+
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
@@ -227,6 +232,9 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
+
+	defer untrace(trace("parseIntegerLiteral"))
+
 	lit := &ast.IntergerLiteral{Token: p.curToken}
 
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
@@ -243,6 +251,9 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
+
+	defer untrace(trace("parsePrefixExpression"))
+
 	expression := &ast.PrefixExpression{
 
 		Token:    p.curToken,
@@ -285,6 +296,9 @@ func (p *Parser) curPrecednce() int {
 }
 
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+
+	defer untrace(trace("parseInfixExpression"))
+
 	expression := &ast.InfixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
