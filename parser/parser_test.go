4d6ad3caef5db4faa641a7468e98ca09ff6b1131
Parthib Mukherjee <parthibmukherjee@gmail.com>
2025-05-03 03:49:48 +0000

    Added(parser, parser_test): functions to parse functionLiterals, more tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -542,8 +542,8 @@ func TestFunctionLiteralParsing(t *testing.T) {
 		t.Fatalf("function literal parameter worng. want 2, got=%T", stmt.Expression)
 	}
 
-	testLiteralExpression(t, function.Parameters[0], 'x')
-	testLiteralExpression(t, function.Parameters[1], 'y')
+	testLiteralExpression(t, function.Parameters[0], "x")
+	testLiteralExpression(t, function.Parameters[1], "y")
 
 	if len(function.Body.Statements) != 1 {
 		t.Fatalf("function.Body.Statements has not 1 statements. got=%d\n", len(function.Body.Statements))
@@ -556,6 +556,34 @@ func TestFunctionLiteralParsing(t *testing.T) {
 	}
 
 	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
+}
+
+func TestFunctionParameterParsing(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedParams []string
+	}{
+		{input: "fn() {};", expectedParams: []string{}},
+		{input: "fn(x) {};", expectedParams: []string{"x"}},
+		{input: "fn(x,y,z) {};", expectedParams: []string{"x", "y", "z"}},
+	}
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+		function := stmt.Expression.(*ast.FunctionLiteral)
+
+		if len(function.Parameters) != len(tt.expectedParams) {
+			t.Errorf("length parameters wrong. want %d, got=%d\n", len(tt.expectedParams), len(function.Parameters))
+		}
+
+		for i, ident := range tt.expectedParams {
+			testLiteralExpression(t, function.Parameters[i], ident)
+		}
+	}
 }
 
 func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
