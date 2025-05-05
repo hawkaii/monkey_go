f1895277f6edc73facb06b72110933a7e627f43b
Parthib Mukherjee <parthibmukherjee@gmail.com>
2025-05-05 17:24:08 +0000

    added(evaluator): added evaluation logic for prefixExpression for "!"

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,11 +26,39 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
 }
 
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// the "!" (bang) operator evaluation saga
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+
+}
 func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 
