42305020215ab80f89dac57c346a3c57e11bf318
Parthib Mukherjee <parthibmukherjee@gmail.com>
2025-05-13 15:58:39 +0000

    Added(evaluator) : Return expression evaluated and tests

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -40,9 +40,27 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+	//adding return Statement
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	}
 
 	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+
+	return result
 }
 
 // function to evaluate conditional statement
@@ -157,15 +175,6 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 
 }
-func evalStatements(stmts []ast.Statement) object.Object {
-	var result object.Object
-
-	for _, statement := range stmts {
-		result = Eval(statement)
-	}
-	return result
-}
-
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
